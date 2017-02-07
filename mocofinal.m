f6ea842a6663c7e2ed686fa83bc3f9df5ac997fe
shockmonger <tejaswineek@gmail.com>
2017-02-07 21:16:22 +0100

    pseudocode

--- mocofinal.m
+++ mocofinal.m
@@ -32,19 +32,44 @@ for k = 1:22
     end
 end
 
-       
-        % select top 32 most far away from each other (sum of rhy)
-temparray = [];
+%separate into arrays for sums, means, sds
 for k = 1:22
-    
-        
-        % find indices for top 32 most far away from each other
-    
-    % send from indices of all top 704 to arrays select1:704 (6 col vector)
+    for j = 1:3636
+        sums.(sprintf('sum%d',k))(j,1) = win.(sprintf('win%d',k)).(sprintf('sums%d',j));
+        stds.(sprintf('std%d',k))(j,1) = win.(sprintf('win%d',k)).(sprintf('std%d',j));
+        means.(sprintf('mean%d',k))(j,1) = win.(sprintf('win%d',k)).(sprintf('mean%d',j));
+    end
+end
+       
+%find peaks per 22 from sums of qom data
+peaks = {};
+locs = {};
+w = {};
+p = {};
+for k = 1:22
+    [peaks.(sprintf('peak%d',k)), locs.(sprintf('loc%d',k)), w.(sprintf('w%d',k)), p.(sprintf('p%d',k))] = findpeaks(sums.(sprintf('sum%d',k)));
+end
+
+
+%peak windows
+peakwins = {};
+for k = 1:22
+   x = length(peaks.(sprintf('peak%d',k)));
+   for j = 1:x
+       numstpID = locs.(sprintf('loc%d',k));
+       pointer = numstpID*slide_incr;
+       point = pointer(j,1);
+       for i = 1:winsize;
+            for l = 1:6
+               peakwins.(sprintf('peakwin%d',k)).(sprintf('sel%d',j))(i,l) = shl.(sprintf('shl%d',k))(i+point,l);    
+            end
+       end
+    end
+end
 
     
 % initialize f.f1-f6 feature vectors for each of the 6 sets of features
-
+f = {};
     % calculate 6x<704x1> vector for f1-f6, f1-f6 are arrays, f is structure
 
         % calculate means for each array for f.f1-f6 (end up with 6 means)
@@ -57,7 +82,8 @@ for k = 1:22
 % decide thresholds
     
 % s.s1-s6 struct.array for means from 1-6 for 704 rows depending on thresholds
-    
+s = {};    
     % means of s1-s6
     
-    % sds of s1-s6
+    % sds of 
+    s1-s6
